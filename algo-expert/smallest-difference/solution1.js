8846a519953ded6707c9d89f63747affee3f2326
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-09-17 14:49:36 -0400

    algo expert: smallest difference solution 1

---
+++ algo-expert/smallest-difference/solution1.js
@@ -0,0 +1,25 @@
+function smallestDifference(arrayOne, arrayTwo) {
+  // Write your code here.
+  arrayOne.sort();
+  arrayTwo.sort();
+
+  const table = {};
+  let closestDiff = null;
+
+  for (let one = 0; one < arrayOne.length; one++) {
+    for (let two = 0; two < arrayTwo.length; two++) {
+      const absDiff = Math.abs(arrayOne[one] - arrayTwo[two]);
+      table[absDiff] = [arrayOne[one], arrayTwo[two]];
+
+      console.log(arrayOne[one], arrayTwo[two], absDiff);
+
+      if (closestDiff === null || absDiff < closestDiff) closestDiff = absDiff;
+    }
+  }
+
+  return table[closestDiff];
+}
+
+// Time: O(n * m), Space: O(n * m)
+// where n is the length of the first input array
+// and m is the length of the second
