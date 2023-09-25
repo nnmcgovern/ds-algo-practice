c9fd8703adfe30ae65e2827eb7bae5f25225b977
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-09-25 14:24:58 -0400

    leetcode zigzag

---
+++ leetcode/zigzag-conversion.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  let zigzag = "";
+  let inc = false;
+  let j = 0;
+  const rows = [];
+
+  for (let i = 0; i < numRows; i++) {
+    rows.push([]);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (j === numRows - 1 || j === 0) inc = !inc;
+
+    rows[j].push(s[i]);
+
+    if (inc) j++;
+    else j--;
+  }
+
+  for (let i = 0; i < numRows; i++) {
+    zigzag += rows[i].join("");
+  }
+
+  return zigzag;
+};
