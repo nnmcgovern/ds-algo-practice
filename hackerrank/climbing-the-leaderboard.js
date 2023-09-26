8af5346405497946d849d93dcdf1546e5e677ad4
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-09-26 15:59:04 -0400

    hackerrank: climbing the leaderboard

---
+++ hackerrank/climbing-the-leaderboard.js
@@ -0,0 +1,22 @@
+function climbingLeaderboard(ranked, player) {
+  // Write your code here
+  let ranks = [];
+
+  for (let i = 0; i < player.length; i++) {
+    let scorePlayer = player[i];
+    let rankCount = 1;
+
+    for (let j = 0; j < ranked.length; j++) {
+      let score = ranked[j];
+      let nextScore = ranked[j + 1];
+
+      if (scorePlayer < score) {
+        if (score !== nextScore) rankCount++;
+      }
+    }
+
+    ranks.push(rankCount);
+  }
+
+  return ranks;
+}
