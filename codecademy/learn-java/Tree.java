bb710df9b8efb9cee84d8709acbd6aee8556e801
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-09-25 23:34:18 -0400

    codecademy learn java: Tree

---
+++ codecademy/learn-java/Tree.java
@@ -0,0 +1,33 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // introduce ourselves and print a tree to the console
+    System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+
+    System.out.println("I'm going to plant a tree today!");
+
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+
+    System.out.println();
+
+    System.out.println(" * ** *");
+    System.out.println(" ******");
+    System.out.println("  **** ");
+    System.out.println("   **  ");
+    System.out.println("   **  ");
+    System.out.println("~~~~~~~~~~");
+
+    System.out.println();
+
+    System.out.println(" *****");
+    System.out.println(" *****");
+    System.out.println(" *****");
+    System.out.println("   *");
+    System.out.println("   *");
+    System.out.println("#######");
+  }
+}
