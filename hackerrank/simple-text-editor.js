9345fd2122aa8525a19ca680b7ee25a56d212ac1
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-09-27 14:34:03 -0400

    hackerrank simple text editor

---
+++ hackerrank/simple-text-editor.js
@@ -0,0 +1,37 @@
+function processData(input) {
+  //Enter your code here
+  let s = "";
+  let strStates = [];
+
+  input = input.split(/\r?\n|\r|\n/g); // split by new lines
+
+  for (let i = 1; i < input.length; i++) {
+    let operation = input[i].split(" ");
+    let opArg = operation[1];
+
+    switch (parseInt(operation[0])) {
+      case 1: // append
+        strStates.push(s);
+        s += opArg;
+        break;
+
+      case 2: // delete chars
+        strStates.push(s);
+        s = s.substring(0, s.length - opArg);
+        break;
+
+      case 3: // print char
+        console.log(s[opArg - 1]);
+        break;
+
+      case 4: // undo
+        s = strStates.pop();
+        break;
+
+      default:
+        console.log(
+          "?"
+        ); /* console log random string because I don't expect to ever execute default */
+    }
+  }
+}
