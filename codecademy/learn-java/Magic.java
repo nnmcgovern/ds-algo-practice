0027f7f3620512d2c0316c10f024171d25b90d95
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-09-27 01:21:06 -0400

    codecademy java magic number

---
+++ codecademy/learn-java/Magic.java
@@ -0,0 +1,20 @@
+public class Magic {
+	public static void main(String[] args) {
+    int myNumber = -46340; // original number
+		int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+    System.out.println(stepSix);
+
+    int magicNumber = myNumber * myNumber;
+    magicNumber += myNumber;
+    magicNumber /= myNumber;
+    magicNumber += 17;
+    magicNumber -= myNumber;
+    magicNumber /= 6;
+    System.out.println(magicNumber);
+	}
+}
