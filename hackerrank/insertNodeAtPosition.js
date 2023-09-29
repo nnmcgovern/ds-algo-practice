6d0bf88f5db1d3eca36bbae05374b45fcb8cea00
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-09-29 14:53:27 -0400

    hackerrank insert node at position linked list

---
+++ hackerrank/insertNodeAtPosition.js
@@ -0,0 +1,40 @@
+// Insert a node at a specific position in a linked list
+
+function insertNodeAtPosition(llist, data, position) {
+  // Write your code here
+  if (position === 0) {
+    return {
+      data: data,
+      next: llist,
+    };
+  }
+
+  // find node that will be ahead of node to be inserted
+  // set that nodes next to new node
+  // new nodes next will be following node
+
+  let newNode = {
+    data: data,
+    next: null,
+  };
+
+  let currPos = 1;
+  let currNode = llist;
+
+  while (currNode) {
+    if (currPos === position) {
+      let prevNode = currNode;
+      let nextNode = prevNode.next;
+      prevNode.next = newNode;
+      newNode.next = nextNode;
+      break;
+    }
+
+    currPos++;
+    currNode = currNode.next;
+  }
+
+  return llist;
+}
+
+// Time: O(n), Space: O(1)
