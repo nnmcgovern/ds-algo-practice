72a22ef946ee9a2923831344f8debb93d07f8a3a
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2024-01-20 12:36:04 -0500

    k points to origin

---
+++ neetcode/heaps-and-priority-queues/k-closest-points-to-origin.py
@@ -0,0 +1,20 @@
+from heapq import *
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        result = []
+
+        for point in points:
+            x, y = point
+
+            distance = x**2 + y**2
+
+            point.insert(0, distance)
+
+        heapify(points)
+
+        for i in range(k):
+            distance, x, y = heappop(points)
+            result.append([x, y])
+
+        return result
