faf0047f09c76c500766a28d8619a8ef69ee1afc
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2024-01-20 12:12:12 -0500

    nnetcode: kth largest

---
+++ neetcode/heaps-and-priority-queues/kth-largest-in-a-stream.py
@@ -0,0 +1,23 @@
+from heapq import * # min heap
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = []
+
+        for num in nums:
+            heappush(self.nums, num)
+
+    def add(self, val: int) -> int:
+        heappush(self.nums, val)
+
+        while len(self.nums) > self.k:
+            heappop(self.nums) # pops smallest num
+
+        return self.nums[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
