755d7d7494fe002b530099df92b7b9626ec62858
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2024-01-20 12:45:19 -0500

    negative

---
+++ neetcode/heaps-and-priority-queues/kth-largest-element-in-an-array/negative.py
@@ -0,0 +1,12 @@
+from heapq import *
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums = [-num for num in nums]
+
+        heapify(nums)
+
+        for i in range(k-1):
+            heappop(nums)
+
+        return heappop(nums) * -1
